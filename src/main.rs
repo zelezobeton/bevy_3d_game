a3751b4caf6fa4362356666d78ddd701d8779740
Demi Jon <pepino.adamek@gmail.com>
2022-07-08 22:05:07 +0200

    Added physics, changed meshes, added player movement using velocity etc.

--- src/main.rs
+++ src/main.rs
@@ -1,249 +1,246 @@
 /*
 TODO:
-- Make character moving gradually, not with steps (add physics)
+- Try adding human mesh to character
 
-- Make custom randomly leveled ground (hills, valleys)
-- Stack cubes, create buildings
 - Spawn creatures that interact with character, chase him, hurt him, etc.
-
-REFACTOR:
-- Default camera position should depend on starting player position
+- Refine player movement
+- Add movement around Y-axis with mouse
 
 DONE:
-- Make camera moving smoother
-- Make the camera follow the character
-- Delete not used features
+- Merge code back to original git repo
+- Make camera focus and follow character
+- Move character with keyboard using velocity
+- Add jump
 */
 
-use bevy::{core::FixedTimestep, ecs::schedule::SystemSet, prelude::*, render::camera::Camera3d};
-use rand::Rng;
+use bevy::prelude::*;
+use bevy::render::mesh::shape as render_shape;
+use bevy_rapier3d::prelude::*;
 
 #[derive(Clone, Eq, PartialEq, Debug, Hash)]
 enum GameState {
     Playing,
-    GameOver,
 }
 
-fn main() {
-    App::new()
-        .insert_resource(Msaa { samples: 4 })
-        .init_resource::<Game>()
-        .add_plugins(DefaultPlugins)
-        .add_state(GameState::Playing)
-        .add_startup_system(setup_cameras)
-        .add_system_set(SystemSet::on_enter(GameState::Playing).with_system(setup))
-        .add_system_set(
-            SystemSet::on_update(GameState::Playing)
-                .with_system(move_player)
-                .with_system(focus_camera)
-        )
-        .add_system_set(SystemSet::on_exit(GameState::Playing).with_system(teardown))
-        .add_system_set(SystemSet::on_exit(GameState::GameOver).with_system(teardown))
-        .add_system(bevy::input::system::exit_on_esc_system)
-        .run();
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct Camera;
+
+// Should I work with entity IDs for these entities?
+// #[derive(Default)]
+// struct Game {
+//     player: Option<Entity>,
+//     camera: Option<Entity>
+// }
+
+const DEFAULT_PLAYER_POS: [f32; 3] = [ 0.0, 1.0, 0.0];
+const DEFAULT_CAMERA_POS: [f32; 3] = [-7.0, 10.0, 0.0];
+
+fn move_camera(
+    mut camera_transform: Query<&mut Transform, (With<Camera>, Without<Player>)>,
+    mut player_transform: Query<&mut Transform, (With<Player>, Without<Camera>)>
+) {
+    let player_pos = player_transform.single_mut().translation;
+
+    let camera_distance: Vec3 = Vec3::from(DEFAULT_CAMERA_POS) - Vec3::from(DEFAULT_PLAYER_POS);
+    let new_camera_pos = player_pos + camera_distance;
+
+    // Interpolated camera movement
+    camera_transform.single_mut().translation = camera_transform.single_mut().translation.lerp(new_camera_pos, 0.2);
 }
 
-struct Cell {
-    height: f32,
+fn move_player(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut velocities: Query<&mut Velocity, With<Player>>
+) {
+    const SPEED: f32 = 7.0;
+    let mut vel = velocities.single_mut();
+    if keyboard_input.pressed(KeyCode::Up) {
+        vel.linvel[0] = SPEED;
+    }
+    else if keyboard_input.pressed(KeyCode::Down) {
+        vel.linvel[0] = -SPEED;
+    }
+    else if keyboard_input.pressed(KeyCode::Left) {
+        vel.linvel[2] = -SPEED;
+    }
+    else if keyboard_input.pressed(KeyCode::Right) {
+        vel.linvel[2] = SPEED;
+    }
+    else {
+        vel.linvel[0] = 0.0;
+        vel.linvel[2] = 0.0;
+    }
+
+    
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        vel.linvel[1] = 5.0;
+    }
+
+    // Custom gravity
+    // vel.linvel[1] -= 1.0;
 }
 
-#[derive(Default)]
-struct Player {
-    entity: Option<Entity>,
-    i: usize,
-    j: usize,
-    move_cooldown: Timer,
+fn setup_camera(mut commands: Commands) {
+    commands
+        .spawn_bundle(PerspectiveCameraBundle {
+            transform: Transform::from_translation(Vec3::from(DEFAULT_CAMERA_POS))
+                .looking_at(Vec3::from(DEFAULT_PLAYER_POS), Vec3::Y), // focus rotation of camera on player
+            ..default()
+        })
+        .insert(Camera);
 }
 
-#[derive(Default)]
-struct Game {
-    board: Vec<Vec<Cell>>,
-    player: Player,
-    camera_is_pos: Vec3,
-    camera_should_pos: Vec3,
-    player_pos: Vec3,
-}
+fn setup_light(mut commands: Commands) {
+    const HALF_SIZE: f32 = 100.0;
 
-const BOARD_SIZE_I: usize = 14;
-const BOARD_SIZE_J: usize = 21;
-
-const DEFAULT_PLAYER_POS: [f32; 3] = [
-    BOARD_SIZE_I as f32 / 2.0,
-    0.0,
-    BOARD_SIZE_J as f32 / 2.0 - 0.5,
-];
-
-const DEFAULT_CAMERA_POS: [f32; 3] = [-7.0, 14.0, 10.0];
-
-fn setup_cameras(mut commands: Commands, mut game: ResMut<Game>) {
-    game.camera_should_pos = Vec3::from(DEFAULT_CAMERA_POS);
-    game.camera_is_pos = game.camera_should_pos; 
-
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_translation(game.camera_is_pos)
-        .looking_at(Vec3::from(DEFAULT_PLAYER_POS), Vec3::Y), // focus rotation of camera on player
-        ..default()
-    });
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut game: ResMut<Game>) {
-    // reset the game state
-    game.player.i = BOARD_SIZE_I / 2;
-    game.player.j = BOARD_SIZE_J / 2;
-    game.player.move_cooldown = Timer::from_seconds(0.3, false);
-
-    commands.spawn_bundle(PointLightBundle {
-        transform: Transform::from_xyz(4.0, 10.0, 4.0),
-        point_light: PointLight {
-            intensity: 3000.0,
+    commands.spawn_bundle(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            illuminance: 10000.0,
+            // Configure the projection to better fit the scene
+            shadow_projection: OrthographicProjection {
+                left: -HALF_SIZE,
+                right: HALF_SIZE,
+                bottom: -HALF_SIZE,
+                top: HALF_SIZE,
+                near: -10.0 * HALF_SIZE,
+                far: 100.0 * HALF_SIZE,
+                ..default()
+            },
             shadows_enabled: true,
-            range: 30.0,
+            ..default()
+        },
+        transform: Transform {
+            translation: Vec3::new(10.0, 2.0, 10.0),
+            rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_4),
             ..default()
         },
         ..default()
     });
+}
 
-    // spawn the game board
-    // let cell_scene = asset_server.load("models/AlienCake/tile.glb#Scene0");
-    let cell_scene = asset_server.load("models/AlienCake/cliff_block_rock.glb#Scene0");
-    game.board = (0..BOARD_SIZE_J)
-        .map(|j| {
-            (0..BOARD_SIZE_I)
-                .map(|i| {
-                    let height = rand::thread_rng().gen_range(-0.3..0.3);
-                    // let height = 0.0;
-                    commands
-                        .spawn_bundle(TransformBundle::from(Transform::from_xyz(
-                            i as f32,
-                            height - 1.0,
-                            j as f32,
-                        )))
-                        .with_children(|cell| {
-                            cell.spawn_scene(cell_scene.clone());
-                        });
-                    Cell { height }
-                })
-                .collect()
+fn spawn_ground(
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+    commands: &mut Commands
+) {
+    let ground_size = 20.1;
+    let ground_height = 0.1;
+
+    let ground = commands
+        .spawn()
+        .insert_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(render_shape::Box::new(
+                ground_size,
+                ground_height,
+                ground_size,
+            ))),
+            // material	: materials.add(Color::rgb(0.8, 0.8, 0.8).into()),
+            material: materials.add(Color::DARK_GREEN.into()),
+            transform: Transform::from_xyz(0.0, -ground_height, 0.0),
+            ..Default::default()
         })
-        .collect();
-
-    // spawn the game character
-    game.player_pos = Vec3::new(
-        game.player.i as f32,
-        game.board[game.player.j][game.player.i].height,
-        game.player.j as f32,
-    );
-    game.player.entity = Some(
+        .insert(Collider::cuboid(ground_size/2.0, ground_height/2.0, ground_size/2.0))
+        .insert(Transform::from_xyz(0.0, -ground_height, 0.0))
+        .insert(GlobalTransform::default())
+        .id();
+    
+    let cube = render_shape::Cube::default();
+    let cubes_positions = [[2.0, 1.0, 2.0], [-2.0, 1.0, 2.0], [2.0, 1.0, -2.0], [-2.0, 1.0, -2.0]];
+    for pos in cubes_positions {
+        let vec_pos = Vec3::from(pos);
         commands
-            .spawn_bundle(TransformBundle::from(Transform {
-                translation: game.player_pos,
-                rotation: Quat::from_rotation_y(-std::f32::consts::FRAC_PI_2),
+            .spawn()
+            .insert_bundle	(PbrBundle {
+                mesh		: meshes.add(Mesh::from(cube)),
+                material	: materials.add(Color::CRIMSON.into()),
+                transform	: Transform::from_xyz(0.0, 1.0, 0.0),
                 ..default()
-            }))
-            .with_children(|cell| {
-                // cell.spawn_scene(asset_server.load("models/AlienCake/alien.glb#Scene0"));
-                cell.spawn_scene(asset_server.load("models/AlienCake/characterDigger.glb#Scene0"));
             })
-            .id(),
-    );
-}
-
-// remove all entities that are not a camera
-fn teardown(mut commands: Commands, entities: Query<Entity, Without<Camera>>) {
-    for entity in entities.iter() {
-        commands.entity(entity).despawn_recursive();
+            .insert(RigidBody::Dynamic)
+            .insert(Collider::cuboid(0.5, 0.5, 0.5))
+            .insert(Restitution::coefficient(0.7))
+            .insert_bundle(TransformBundle::from(Transform::from_translation(vec_pos)));
     }
+
+    let sphere = render_shape::Capsule {
+        depth: 0.0,
+        ..default()
+    };
+    commands
+        .spawn()
+        .insert_bundle	(PbrBundle {
+            mesh		: meshes.add(Mesh::from(sphere)),
+            material	: materials.add(Color::GOLD.into()),
+            transform	: Transform::from_xyz(0.0, 1.0, 0.0),
+            ..default()
+        })
+        .insert(RigidBody::Dynamic)
+        .insert(Collider::ball(0.5))
+        .insert(Restitution::coefficient(0.7))
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(1.0, 0.0, 1.0)));
 }
 
-// control the game character
-fn move_player(
-    mut commands: Commands,
-    keyboard_input: Res<Input<KeyCode>>,
-    mut game: ResMut<Game>,
-    mut transforms: Query<&mut Transform>,
-    time: Res<Time>,
+fn spawn_player(
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+    commands: &mut Commands,
 ) {
-    if game.player.move_cooldown.tick(time.delta()).finished() {
-        let mut moved = false;
-        let mut rotation = 0.0;
-
-        if keyboard_input.pressed(KeyCode::Up) {
-            if game.player.i < BOARD_SIZE_I - 1 {
-                game.player.i += 1;
-            }
-            rotation = -std::f32::consts::FRAC_PI_2;
-            moved = true;
-        }
-        if keyboard_input.pressed(KeyCode::Down) {
-            if game.player.i > 0 {
-                game.player.i -= 1;
-            }
-            rotation = std::f32::consts::FRAC_PI_2;
-            moved = true;
-        }
-        if keyboard_input.pressed(KeyCode::Right) {
-            if game.player.j < BOARD_SIZE_J - 1 {
-                game.player.j += 1;
-            }
-            rotation = std::f32::consts::PI;
-            moved = true;
-        }
-        if keyboard_input.pressed(KeyCode::Left) {
-            if game.player.j > 0 {
-                game.player.j -= 1;
-            }
-            rotation = 0.0;
-            moved = true;
-        }
-
-        // move on the board
-        if moved {
-            game.player.move_cooldown.reset();
-            *transforms.get_mut(game.player.entity.unwrap()).unwrap() = Transform {
-                translation: Vec3::new(
-                    game.player.i as f32,
-                    game.board[game.player.j][game.player.i].height,
-                    game.player.j as f32,
-                ),
-                rotation: Quat::from_rotation_y(rotation),
-                ..default()
-            };
-        }
-    }
+    let capsule = render_shape::Capsule {
+        depth: 1.0,
+        ..default()
+    };
+    commands
+        .spawn()
+        .insert(Player)
+        .insert_bundle	(PbrBundle {
+			mesh		: meshes.add(Mesh::from(capsule)),
+			material	: materials.add(Color::NAVY.into()),
+			transform	: Transform::from_xyz(0.0, 1.0, 0.0),
+			..Default::default()
+		})
+        // .insert_bundle(TransformBundle {
+        //     local: Transform::from_xyz(0.0, 1.0, 0.0),
+        //     global: GlobalTransform::identity(),
+        // })
+        // .with_children(|cell| {
+        //     cell.spawn_scene(asset_server.load("models/AlienCake/characterDigger.glb#Scene0"));
+        // })
+        .insert(RigidBody::Dynamic)
+        .insert(Velocity::zero())
+        .insert(LockedAxes::ROTATION_LOCKED)
+        // .insert(GravityScale(0.0)) // May be needed when tweaking movement for RigidBody::Dynamic
+        // .insert(Collider::ball(0.5))
+        .insert(Collider::capsule_y(0.5, 0.5))
+        .insert(Restitution::coefficient(0.7));
 }
 
-// change the focus of the camera
-fn focus_camera(
-    time: Res<Time>,
-    mut game: ResMut<Game>,
-    mut transforms: ParamSet<(Query<&mut Transform, With<Camera3d>>, Query<&Transform>)>,
+fn setup_physics(
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+    mut commands: Commands
 ) {
-    // Target player with camera if player exists
-    if let Some(player_entity) = game.player.entity {
-        if let Ok(player_transform) = transforms.p1().get(player_entity) {
-            let player_new_pos = player_transform.translation;
-            let player_pos_diff = game.player_pos - player_new_pos;
-            game.player_pos = player_new_pos;
-            game.camera_should_pos = game.camera_should_pos - player_pos_diff;
-        }
-    // otherwise, target the middle of the board
-    } else {
-        game.camera_should_pos = Vec3::from(DEFAULT_CAMERA_POS);
-    }
+    spawn_ground(&mut meshes, &mut materials, &mut commands);
+    spawn_player(&mut meshes, &mut materials, asset_server, &mut commands);
+}
 
-    // Smooth movement #1
-    // game.camera_is_pos = game.camera_is_pos.lerp(game.camera_should_pos, 0.2);
-
-    // Smooth movement #2
-    const SPEED: f32 = 2.0;
-    let mut camera_motion = game.camera_should_pos - game.camera_is_pos;
-    if camera_motion.length() > 0.2 {
-        camera_motion *= SPEED * time.delta_seconds();
-        game.camera_is_pos += camera_motion;
-    }
-
-    // look at that new camera's actual focus
-    for mut transform in transforms.p0().iter_mut() {
-        *transform = transform.with_translation(game.camera_is_pos);
-    }
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        // .add_plugin(RapierDebugRenderPlugin::default())
+        .add_state(GameState::Playing)
+        .add_startup_system(setup_camera)
+        .add_startup_system(setup_light)
+        .add_startup_system(setup_physics)
+        .add_system_set(
+            SystemSet::on_update(GameState::Playing)
+                .with_system(move_player)
+                .with_system(move_camera)
+        )
+        .run();
 }
